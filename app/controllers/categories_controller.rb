2d11642ded6e0e31f834774fbd84877447a0da4a
naetoru9393 <haruya.maeda@prum.jp>
2023-11-26 09:51:46 +0900

    Implement basic login

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,29 @@
 class CategoriesController < ApplicationController
+
     def show
+        @category = Category.all
         @item = Item.all
     end
+
+    def new
+        @category = Category.new
+        @item = Item.new
+      end
+    
+      def create
+          @category = Category.new(category_params)
+          @category.save
+        end
+      
+        def update
+            @category = Category.find(category_params)
+            @category.update(category_params)
+        end
+
+    private
+
+    def category_params
+      params.require(:category).permit(:category_name, :category_id)
+    end
+
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
 
   def new
+    @category = Category.find(category_params)
+    @category.save
     @item = Item.new
   end
 
@@ -18,5 +20,9 @@ class ItemsController < ApplicationController
 
     def item_params
       params.require(:item).permit(:item_name, :study_time, :category_id)
+    end
+
+    def category_params
+      params.require(:category).permit(:category_name, :category_id)
     end
   end
