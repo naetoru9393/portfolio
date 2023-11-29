547b5d447cabe15b1970b5768b24c441ab7bf53b
naetoru9393 <haruya.maeda@prum.jp>
2023-11-29 12:16:09 +0900

    Implement basic login

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -33,13 +33,3 @@ class ItemsController < ApplicationController
                                    :category_id, :item_id)
     end
   end
-
-   # beforeフィルタ
-
-    # ログイン済みユーザーかどうか確認
-    def logged_in_user
-      unless logged_in?
-        flash[:danger] = "Please log in."
-        redirect_to login_url, status: :see_other
-      end
-end
