1e4acfc1ae0572d4e12009d9e8b57255dcfa90d9
naetoru9393 <haruya.maeda@prum.jp>
2023-12-28 15:16:36 +0900

    Make major mistake

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,6 @@ class UsersController < ApplicationController
         if @user.save
           reset_session
           log_in @user
-          flash[:success] = "Welcome to the Sample App!"
           redirect_to @user
         else
           render 'new'
